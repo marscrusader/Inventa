929020c9a86c120098d71674a327938e85e04273
Shawn Phua <shawnphua27@gmail.com>
2021-01-19 11:37:01 +0800

    make auth work

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 import logo from './logo.svg'
 import './App.css'
 import { useAuth0 } from '@auth0/auth0-react'
-import LoginButton from '../components/common/loginButton'
-import LogoutButton from '../components/common/logoutButton'
+import LoginButton from './components/common/loginButton'
+import LogoutButton from './components/common/logoutButton'
 
 export default function App(): JSX.Element {
   const { isAuthenticated } = useAuth0()
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router } from "react-router-dom";
-import Auth0ProviderWithHistory from "../auth/auth0-provider";
+import Auth0ProviderWithHistory from "./auth/auth0-provider";
 
 ReactDOM.render(
   <Router>
