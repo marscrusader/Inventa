ee2cb40f91231493693065e1221302d2751f3b08
Shawn Phua <shawn2798@icloud.com>
2021-01-27 15:47:50 +0800

    db prod

--- backend/database.ts
+++ backend/database.ts
@@ -3,18 +3,34 @@ import dotenv from 'dotenv'
 
 dotenv.config()
 let dbConnection: Sequelize | null = null
-if (process.env.POSTGRES_DB && process.env.POSTGRES_USER && process.env.POSTGRES_PASSWORD) {
-  dbConnection = new sequelize.Sequelize(
-    process.env.POSTGRES_DB,
-    process.env.POSTGRES_USER,
-    process.env.POSTGRES_PASSWORD,
-    {
-      host: process.env.POSTGRES_HOST,
-      dialect: 'postgres'
-    }
-  );
+if (process.env.NODE_ENV === 'production') {
+  if (process.env.DATABASE_URL) {
+    dbConnection = new sequelize.Sequelize(
+      process.env.DATABASE_URL,
+      {
+        dialect: 'postgres',
+        dialectOptions: {
+          ssl: {
+            require: true,
+            rejectUnauthorized: false
+          }
+        }
+      }
+    );
+  }
+} else {
+  if (process.env.POSTGRES_DB && process.env.POSTGRES_USER && process.env.POSTGRES_PASSWORD) {
+    dbConnection = new sequelize.Sequelize(
+      process.env.POSTGRES_DB,
+      process.env.POSTGRES_USER,
+      process.env.POSTGRES_PASSWORD,
+      {
+        host: process.env.POSTGRES_HOST,
+        dialect: 'postgres'
+      }
+    );
+  }
 }
-
 if (!dbConnection) throw 'Database is not set up'
 
 export default (dbConnection as Sequelize)
